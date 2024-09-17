3061290eb9ca334cc7a8a1f413dd3ff69df5d753
Tom Hamming <tom.hamming@gmail.com>
2024-09-17 13:57:09 -0700

    Initial state

---
+++ ControlWidgets/ControlWidgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  ControlWidgetsBundle.swift
+//  ControlWidgets
+//
+//  Created by Tom Hamming on 9/17/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ControlWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        ControlWidgets()
+        ControlWidgetsControl()
+    }
+}
