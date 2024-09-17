ed1b8cf2a216d07ba2c00cb437bf2a6fad93c810
Tom Hamming <tom.hamming@gmail.com>
2024-09-17 14:40:08 -0700

    Open the app from control center!

--- ControlWidgets/ControlWidgetsBundle.swift
+++ ControlWidgets/ControlWidgetsBundle.swift
@@ -13,5 +13,6 @@ struct ControlWidgetsBundle: WidgetBundle {
     var body: some Widget {
         ControlWidgets()
         ControlWidgetsControl()
+        LaunchAppControl()
     }
 }
--- ControlWidgets/ControlWidgetsControl.swift
+++ ControlWidgets/ControlWidgetsControl.swift
@@ -28,6 +28,31 @@ struct ControlWidgetsControl: ControlWidget {
     }
 }
 
+struct LaunchAppControl : ControlWidget {
+    var body : some ControlWidgetConfiguration {
+        StaticControlConfiguration(kind: "com.olivetree.ControlWidgetTester.LaunchAppControl") {
+            ControlWidgetButton(action: LaunchAppIntent()) {
+                Image(systemName: "book.pages")
+            }
+        }
+        .displayName("Launch App")
+    }
+}
+
+struct LaunchAppIntent: ControlConfigurationIntent {
+    static let title: LocalizedStringResource = "Launch App"
+    static let description = IntentDescription(stringLiteral: "Launch the app!")
+    static let isDiscoverable = false
+    
+    static let openAppWhenRun: Bool = true
+    
+    @MainActor
+    func perform() async throws -> some IntentResult & OpensIntent {
+        let url = URL(string: "otcontrolwidget://test")!
+        return .result(opensIntent: OpenURLIntent(url))
+    }
+}
+
 extension ControlWidgetsControl {
     struct Provider: ControlValueProvider {
         var previewValue: Bool {
